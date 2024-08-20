5b48409210d8304c7e8858718a2898b86826f33c
Ryan <ryanhyukkim@gmail.com>
2024-08-19 17:56:22 -0700

    Button animation

--- Calc/Presentation Layer/UIKit Entensions/UIButton+Animations.swift
+++ Calc/Presentation Layer/UIKit Entensions/UIButton+Animations.swift
@@ -10,31 +10,18 @@ import UIKit
 
 extension UIButton {
     func bounce() {
-        moveUp()
+        moveUpWithSpringDamping()
     }
     
-    func growLarger() {
-        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut) {
-            self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+    func moveUpWithSpringDamping() {
+        
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseOut, .allowUserInteraction]) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
         } completion: { _ in
-        }
-    }
-    
-    func moveUp() {
-        let imageView = UIImageView()
-        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseIn) {
-            self.transform = CGAffineTransform(translationX: 0, y: -50)
-        } completion: { _ in
-            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseIn) {
-                self.transform = CGAffineTransform(translationX: 0, y: 0)
+            UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 5, options: [.curveEaseInOut, .allowUserInteraction]) { [weak self] in
+                self?.transform = CGAffineTransform.identity
             } completion: { _ in
-                
-                
             }
-
         }
-
     }
-    
-    
 }
